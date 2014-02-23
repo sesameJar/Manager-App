c5cf764fae3060d0a112f8e03082d4c393bb9abf
mkdesign <me@mkdesign.ir>
2014-02-23 12:54:52 +0330

    using function to fix a bug:bug for last and first in next and pre clicking

--- scripts.js
+++ scripts.js
@@ -1,29 +1,29 @@
 $(function () {
 	var player = $('audio'),
-		songs = $('div.songlist > a'),
+		songs = $('div.songlist a'),
 		nextAudio = $('button#next'),
 		preAudio = $('button#pre'),
 		stopAudio = $('button#stop'),
 		playAudio = $('button#play'),
-		cnt=0;
+		currentSong=0;
+
+		(play =function(n){
+			if(n>songs.length-1) n=0;
+			if(n<0) n=songs.length-1;
+			player.attr('src',songs.eq(n).attr('href'));
+			currentSong=n;
+		})(0);
 
 	nextAudio.click(function(){
-		if(cnt==songs.length)
-			cnt=0;
-		else cnt++;
-		player.attr('src',songs.eq(cnt).attr('href'));
+		play(currentSong+1);
 	});
 	stopAudio.click(function(){
 		player.attr('src','');
 	});
 	playAudio.click(function(){
-		player.attr('src',songs.eq(cnt).attr('href'));
+		play(currentSong);
 	});
 	preAudio.click(function(){
-		if(cnt==0)
-			cnt=songs.length;
-		else cnt--;
-
-		player.attr('src',songs.eq(cnt).attr('href'));
+		play(currentSong-1);
 	});
 })
