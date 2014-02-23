1104a05be97d133e313d5107428619a57fdf4101
mkdesign <me@mkdesign.ir>
2014-02-23 12:41:51 +0330

    player finished

--- scripts.js
+++ scripts.js
@@ -1,13 +1,29 @@
 $(function () {
-	var player = $('audio');
-	var songs = $('div.songlist > a');
-	var nextAudio = $('button#next');
-	var cnt=0;
+	var player = $('audio'),
+		songs = $('div.songlist > a'),
+		nextAudio = $('button#next'),
+		preAudio = $('button#pre'),
+		stopAudio = $('button#stop'),
+		playAudio = $('button#play'),
+		cnt=0;
 
 	nextAudio.click(function(){
-		cnt++;
-		alert(songs.eq(cnt).attr('href'));
+		if(cnt==songs.length)
+			cnt=0;
+		else cnt++;
 		player.attr('src',songs.eq(cnt).attr('href'));
-	})
+	});
+	stopAudio.click(function(){
+		player.attr('src','');
+	});
+	playAudio.click(function(){
+		player.attr('src',songs.eq(cnt).attr('href'));
+	});
+	preAudio.click(function(){
+		if(cnt==0)
+			cnt=songs.length;
+		else cnt--;
 
+		player.attr('src',songs.eq(cnt).attr('href'));
+	});
 })
